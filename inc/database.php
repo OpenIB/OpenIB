1ccdd71ba1e9f6feda62c73728e1170cc845134e
Sardach <sardach@users.noreply.github.com>
2018-08-19 17:32:19 -0500

    recovering debug mode

--- inc/database.php
+++ inc/database.php
@@ -6,10 +6,55 @@
 
 defined('TINYBOARD') or exit;
 
+class PreparedQueryDebug {	
+	protected $query, $explain_query = false;	
+		
+	public function __construct($query) {	
+		global $pdo, $config;	
+		$query = preg_replace("/[\n\t]+/", ' ', $query);	
+			
+		$this->query = $pdo->prepare($query);	
+		if ($config['debug'] && $config['debug_explain'] && preg_match('/^(SELECT|INSERT|UPDATE|DELETE) /i', $query))	
+			$this->explain_query = $pdo->prepare("EXPLAIN $query");	
+	}	
+	public function __call($function, $args) {	
+		global $config, $debug;	
+			
+		if ($config['debug'] && $function == 'execute') {	
+			if ($this->explain_query) {	
+				$this->explain_query->execute() or error(db_error($this->explain_query));	
+			}	
+			$start = microtime(true);	
+		}	
+			
+		if ($this->explain_query && $function == 'bindValue')	
+			call_user_func_array(array($this->explain_query, $function), $args);	
+			
+		$return = call_user_func_array(array($this->query, $function), $args);	
+			
+		if ($config['debug'] && $function == 'execute') {	
+			$time = microtime(true) - $start;	
+			$debug['sql'][] = array(	
+				'query' => $this->query->queryString,	
+				'rows' => $this->query->rowCount(),	
+				'explain' => $this->explain_query ? $this->explain_query->fetchAll(PDO::FETCH_ASSOC) : null,	
+				'time' => '~' . round($time * 1000, 2) . 'ms'	
+			);	
+			$debug['time']['db_queries'] += $time;	
+		}	
+			
+		return $return;	
+	}	
+}	
+
 function sql_open() {
-	global $pdo, $config;
+	global $pdo, $config, $debug;	
 	if ($pdo)
 		return true;
+			
+	if ($config['debug'])	
+		$start = microtime(true);	
+	
 	
 	if (isset($config['db']['server'][0]) && $config['db']['server'][0] == ':')
 		$unix_socket = substr($config['db']['server'], 1);
@@ -29,6 +74,9 @@ function sql_open() {
 		if ($config['db']['persistent'])
 			$options[PDO::ATTR_PERSISTENT] = true;
 		$pdo = new PDO($dsn, $config['db']['user'], $config['db']['password'], $options);
+		if ($config['debug'])	
+			$debug['time']['db_connect'] = '~' . round((microtime(true) - $start) * 1000, 2) . 'ms';	
+		
 		
 		if (mysql_version() >= 50503)
 			query('SET NAMES utf8mb4') or error(db_error());
@@ -63,21 +111,42 @@ function mysql_version() {
 }
 
 function prepare($query) {
-	global $pdo, $config;
+	global $pdo, $debug, $config;	
 	
 	$query = preg_replace('/``('.$config['board_regex'].')``/u', '`' . $config['db']['prefix'] . '$1`', $query);
 	
 	sql_open();
+	if ($config['debug'])	
+		return new PreparedQueryDebug($query);	
 	
 	return $pdo->prepare($query);
 }
 
 function query($query) {
-	global $pdo, $config;
+	global $pdo, $debug, $config;	
 	
 	$query = preg_replace('/``('.$config['board_regex'].')``/u', '`' . $config['db']['prefix'] . '$1`', $query);
 	
 	sql_open();
+	
+	if ($config['debug']) {	
+		if ($config['debug_explain'] && preg_match('/^(SELECT|INSERT|UPDATE|DELETE) /i', $query)) {	
+			$explain = $pdo->query("EXPLAIN $query") or error(db_error());	
+		}	
+		$start = microtime(true);	
+		$query = $pdo->query($query);	
+		if (!$query)	
+			return false;	
+		$time = microtime(true) - $start;	
+		$debug['sql'][] = array(	
+			'query' => $query->queryString,	
+			'rows' => $query->rowCount(),	
+			'explain' => isset($explain) ? $explain->fetchAll(PDO::FETCH_ASSOC) : null,	
+			'time' => '~' . round($time * 1000, 2) . 'ms'	
+		);	
+		$debug['time']['db_queries'] += $time;	
+		return $query;	
+	}	
 	
 	return $pdo->query($query);
 }
